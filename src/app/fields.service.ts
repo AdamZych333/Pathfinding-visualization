7b989c39c95a8d8aeb56d734f3a7ea3d3ae2f915
AdamZych333 <adam.zych99@wp.pl>
2022-07-02 15:18:25 +0200

    added field service

---
+++ src/app/fields.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Color } from './settings.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FieldsService {
+  readonly WIDTH: number = 70;
+  readonly HEIGHT: number = 20;
+  fields: string[][] = [];
+
+  constructor() { 
+    for(let i = 0; i < this.HEIGHT; i++){
+      this.fields[i] = [];
+      for(let j = 0; j < this.WIDTH; j++){
+        this.fields[i][j] = Color.EMPTY;
+      }
+    }
+    this.fields[0][0] = Color.START;
+    this.fields[this.HEIGHT-1][this.WIDTH-1] = Color.END;
+  }
+
+  getFields(): string[][]{
+    return this.fields;
+  }
+
+  getField(x: number, y: number){
+    return this.fields[x][y];
+  }
+
+  setField(x: number, y: number, color: string){
+    if(this.fields[x][y] == Color.START || this.fields[x][y] == Color.END) return;
+    if(color === Color.START){
+      const start = this.findField(e => e == Color.START);
+      if(start !== null) this.fields[start.x][start.y] = Color.EMPTY;
+    }
+    else if(color === Color.END){
+      const end = this.findField(e => e == Color.END);
+      if(end !== null) this.fields[end.x][end.y] = Color.EMPTY;
+    }
+    this.fields[x][y] = color;
+  }
+
+  findField(condition: (field: string) => boolean){
+    for(let i = 0; i < this.HEIGHT; i++){
+      for(let j = 0; j < this.WIDTH; j++){
+        if(condition(this.fields[i][j])) return {x: i, y: j};
+      }
+    }
+    return null;
+  }
+}
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
+import { FieldsService } from '../fields.service';
 import { Color, SettingsService } from '../settings.service';
 
 @Component({
@@ -6,47 +7,20 @@ import { Color, SettingsService } from '../settings.service';
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.sass']
 })
-export class GridComponent implements OnInit {
-  readonly WIDTH: number = 70;
-  readonly HEIGHT: number = 20;
+export class GridComponent {  
   fields: string[][] = [];
 
-  constructor(private settingsService: SettingsService) {}
-
-  ngOnInit(): void {
-    this.newBoard();
-  }
-  
-  newBoard(){
-    for(let i = 0; i < this.HEIGHT; i++){
-      this.fields[i] = [];
-      for(let j = 0; j < this.WIDTH; j++){
-        this.fields[i][j] = Color.EMPTY;
-      }
-    }
+  constructor(private settingsService: SettingsService, private fieldsService: FieldsService) {
+    this.fields = fieldsService.getFields();
   }
 
   onFieldClick(x: number, y: number){
     const selectedOption = this.settingsService.getSelectedPlaceable();
     const type = selectedOption != null? selectedOption.type: '';
-    if(selectedOption?.value === 'start'){
-      const start = this.findField(e => e == Color.START);
-      if(start !== null) this.fields[start.x][start.y] = Color.EMPTY;
-    }
-    else if(selectedOption?.value === 'end'){
-      const start = this.findField(e => e == Color.END);
-      if(start !== null) this.fields[start.x][start.y] = Color.EMPTY;
-    }
-    this.fields[x][y] = type != undefined? type: this.fields[x][y];
+    if(type != undefined)
+      this.fieldsService.setField(x, y, type);
   }
 
-  private findField(condition: (field: string) => boolean){
-    for(let i = 0; i < this.HEIGHT; i++){
-      for(let j = 0; j < this.WIDTH; j++){
-        if(condition(this.fields[i][j])) return {x: i, y: j};
-      }
-    }
-    return null;
-  }
+  
 
 }
